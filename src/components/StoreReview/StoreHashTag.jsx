61ccb2b7b4445c04a6269d8425dedab3708a5996
osy <osy9757@gmail.com>
2024-02-22 10:42:39 +0900

    chore: changed hashtag id

--- src/components/StoreReview/StoreHashTag.jsx
+++ src/components/StoreReview/StoreHashTag.jsx
@@ -13,19 +13,19 @@ function StoreHashTag() {
     <div className="border-b-8">
       <p className="m-4 font-bold ">이런점이 좋았어요</p>
       <div className="my-2">
-        <HashtagElement id={7} count={245} />
+        <HashtagElement id={206} count={245} />
       </div>
       <div className="my-2">
-        <HashtagElement id={8} count={105} />
+        <HashtagElement id={207} count={105} />
       </div>
       <div className="my-2">
-        <HashtagElement id={6} count={5} />
+        <HashtagElement id={105} count={5} />
       </div>
       <div className="my-2">
-        <HashtagElement id={10} count={3} />
+        <HashtagElement id={209} count={3} />
       </div>
       <div className="mt-2 mb-8">
-        <HashtagElement id={1} count={1} />
+        <HashtagElement id={101} count={1} />
       </div>
     </div>
   );
