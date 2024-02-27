0169ed93d672068b884d88bb0d78a41abfea80ae
tori <sangeun99@khu.ac.kr>
2024-02-27 10:06:52 +0900

    chore: 환경변수로 대체

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -9,8 +9,8 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  // process.env.REACT_APP_BACK_SERVER_URL
-  const target = "http://localhost:8080";
+  
+  const target = process.env.REACT_APP_API_BASE_URL;
 
   app.use(
     "/api/v1",
