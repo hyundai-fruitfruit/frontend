21c5d7a6e7e178c6b448fc2838cda3be298ea340
sooyoung <sooyoung.h8@gmail.com>
2024-02-28 15:32:51 +0900

    feat: 누락된 경로 추가

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@
  */
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import MyRoom from './pages/MyRoom/MyRoom';
 import KakaoCallback from './pages/Login/KakaoCallback';
 import MainPage from './pages/MainPage/Mainpage';
 import LocationPage from './pages/Location/LocationPage';
