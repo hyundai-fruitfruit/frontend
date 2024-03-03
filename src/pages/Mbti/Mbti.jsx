d48ff7c4073ea2a80ce15ff9de470a94163a45f5
tori <sangeun99@khu.ac.kr>
2024-03-03 20:54:58 +0900

    refactor: 첫 화면부터 main 화면까지 연결

--- src/pages/Mbti/Mbti.jsx
+++ src/pages/Mbti/Mbti.jsx
@@ -9,47 +9,49 @@ import SpeechBubble from 'components/SpeechBubble/SpeechBubble';
 import CouponCard2 from 'components/CouponCard/CouponCard2';
 import ModalHeendy from 'assets/images/modal_heendy.png';
 import Header from 'components/Header/Header';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import mbtiContent from 'assets/data/mbtiContent';
 
-  const Mbti = () => {
-    const [currentIndex, setCurrentIndex] = useState(0); 
-    
-    const handleNextContent = () => {
-      if (currentIndex < mbtiContent.length - 1) {
-        setCurrentIndex(currentIndex + 1);
-      } else {
-        setCurrentIndex(0); // 마지막 컨텐츠 도달
-      }
-    };
-    return (
-      <>
-      <div className='mt-10 ml-4'>
-        <Header/>
-        </div>
-          <div className="flex flex-col items-center justify-center h-screen mt-[-20%]">
-            <SpeechBubble arrowPostion="right" backgroundColor="#FAFAFA" padding={'20px'} height={'50%'} width={'80%'}>
-              <div className="my-10">
-                <p className={`text-center ${currentIndex === 0 ? 'font-bold' : 'font-normal'}`}>
-                  더현대 서울에서 쇼핑한다면
-                </p>
-                </div>
-              {mbtiContent[currentIndex].map((item, index) => (
-              <CouponCard2
-                key={index}
-                imgSrc={item.imgSrc}
-                detail={item.detail}
-                addStyle="bg-white"
-                onClick={handleNextContent}
-              />
-            ))}
-            </SpeechBubble>
-            <div className="flex justify-end w-full mt-4">
-            <img src={ModalHeendy} alt="Heendy" className="h-[12vh]" />
-            </div>
-        </div>
-      </>
-    );
-  }
+const Mbti = () => {
+  const navigate = useNavigate();
+  const [currentIndex, setCurrentIndex] = useState(0); 
   
-  export default Mbti;
+  const handleNextContent = () => {
+    if (currentIndex < mbtiContent.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      navigate('/main');
+    }
+  };
+  return (
+    <>
+    <div className='mt-10 ml-4'>
+      <Header/>
+      </div>
+        <div className="flex flex-col items-center justify-center h-screen mt-[-20%]">
+          <SpeechBubble arrowPostion="right" backgroundColor="#FAFAFA" padding={'20px'} height={'50%'} width={'80%'}>
+            <div className="my-10">
+              <p className={'text-center font-bold'}>
+                더현대 서울에서 쇼핑한다면
+              </p>
+              </div>
+            {mbtiContent[currentIndex].map((item, index) => (
+            <CouponCard2
+              key={index}
+              imgSrc={item.imgSrc}
+              detail={item.detail}
+              addStyle="bg-white"
+              onClick={handleNextContent}
+            />
+          ))}
+          </SpeechBubble>
+          <div className="flex justify-end w-full mt-4">
+          <img src={ModalHeendy} alt="Heendy" className="h-[12vh]" />
+          </div>
+      </div>
+    </>
+  );
+}
+
+export default Mbti;
