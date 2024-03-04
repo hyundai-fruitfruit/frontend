86e81b9a1ca0614ed21d8e5d3aa533bad014ba3a
sooyoung <sooyoung.h8@gmail.com>
2024-03-04 21:28:58 +0900

    누락된 파일 추가

--- src/pages/RandomEventSpot/EventDetail.jsx
+++ src/pages/RandomEventSpot/EventDetail.jsx
@@ -6,18 +6,20 @@
  * @desc 랜덤 스팟 - 이벤트 상세
  */
 import React from 'react';
-import EventBanner from '../../assets/images/event3.png';
- // import Event from '../../assets/images/event2.png';
+// import EventBanner from '../../assets/images/event3.png';
+import EventBanner from '../../assets/images/event1.png';
+import StoreBanner from '../../assets/images/event2.png';
+import BtnStore from '../../assets/images/btn_event1.png';
+import BtnEvent from '../../assets/images/btn_event2.png';
 
 // import MainHeader from 'components/Header/MainHeader';
-// import MainHeader from 'components/Header/MainHeader';
-
+import MainHeader from 'components/Header/MainHeader';
+//import BlackButton from '../../components/Button/BlackButton';
 
 function EventDetail() {
     const containerStyle = {
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'flex-start',
         alignItems: 'center',
         height: '100%',
         paddingTop: '20px',
@@ -27,36 +29,49 @@ function EventDetail() {
       backgroundImage: `url(${EventBanner})`,
       backgroundSize: 'cover',
       backgroundPosition: 'center',
-      width: '100%',
-      height: '100vh',
+      width: '100vw',
+      height: '50vh',
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
+      marginTop: '6vh',
+      marginBottom: '1vh',
     };
 
-    // const eventStyle = {
-    //     backgroundImage: `url(${Store})`,
-    //     backgroundSize: 'cover',
-    //     backgroundPosition: 'center',
-    //     width: '100%',
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    // };
+    const eventStyle = {
+        backgroundImage: `url(${StoreBanner})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '80vw',
+        height: '30vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    };
 
-    const routeToMyRoom = async () => {
-        window.location.href = '/myroom';
+    const buttonStyle = {
+        width: '78vw',
+        height: 'auto',
+        marginTop: '2vh',
+        marginBottom: '5vh'
+    };
+    
+    const routeToStoreDetail = async () => {
+        window.location.href = '/storeDetail/2';
+    };
+
+    const routeToEventDetail = async () => {
+        window.location.href = '/couponTab';
     };
 
     return (
       <div style={containerStyle}>
-        {/* <MainHeader /> */}
-        <div style={storeStyle} onClick={routeToMyRoom}>
-        </div>
-
-        {/* <div style={eventStyle}>
-        </div> */}
+        <MainHeader />
+        <div style={storeStyle}></div>
+        {/* 이후 매장 상세는 챗봇에 연결하기! */}
+        <img src={BtnStore} style={buttonStyle} alt="BtnStore" onClick={routeToStoreDetail}/>
+        <div style={eventStyle}></div>
+        <img src={BtnEvent} style={buttonStyle} alt="BtnEvent" onClick={routeToEventDetail} />
       </div>
     );
 }
