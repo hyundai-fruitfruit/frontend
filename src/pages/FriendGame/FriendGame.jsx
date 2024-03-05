af3f6655d2619ae43273c6fa165bc1d1cb67ad23
tori <sangeun99@khu.ac.kr>
2024-03-05 13:19:53 +0900

    feat: 친구 페이지 CSS 수정

--- src/pages/FriendGame/FriendGame.jsx
+++ src/pages/FriendGame/FriendGame.jsx
@@ -21,11 +21,9 @@ import { findFriendList } from 'apis/request';
 import MainHeader from 'components/Header/MainHeader';
 import IconMenu from 'components/IconMenu/IconMenu';
 import BackgroundImage from 'components/BackgroundImage/BackgroundImage';
-import ClaymorphicButton from 'components/ClaymorphicButton/ClaymorphicButton';
 import SpeechBubble from 'components/SpeechBubble/SpeechBubble';
 
 // asset
-import itemIcon from 'assets/images/item_icon.png';
 import heendy_background from 'assets/images/heendy_background.png';
 import mainHeendyImage from 'assets/images/mainHeendyImage.png';
 
@@ -50,19 +48,30 @@ function FriendGame() {
   return (
     <div className="h-full">
       <MainHeader />
-      <div className="mt-20 ">
-      {iconsData && <IconMenu icons={iconsData} />} 
+      <div className="mt-20 h-15">
+        {iconsData && <IconMenu icons={iconsData} />} 
       </div>
       <div>
-        <SpeechBubble />
+        <SpeechBubble boldText={"친구를 선택하고 게임을 시작해봐!"} />
       </div>
       <div className="mb-8 relative w-[85vw] h-[42vh] mx-auto">
         <BackgroundImage imageUrl={heendy_background} />
         <img src={mainHeendyImage} alt="캐릭터" className="absolute inset-0 h-[30vh] w-auto mx-auto my-auto mb-0" />
       </div>
-      <div className="grid grid-cols-2 gap-4 p-4">
-        <ClaymorphicButton imageSrc={itemIcon} text="주사위 게임" onClick={() => navigate('/dice')} />
-        <ClaymorphicButton imageSrc={itemIcon} text="친구 성향 분석" />
+      <div className='border border-gray-200 rounded-xl mx-6 flex flex-col justify-around items-center h-52'>
+        <p>흰순이와 게임할래?</p>
+        <div className='flex justify-between items-center w-full px-7'>
+          <div className='border border-gray-200 rounded-2xl flex flex-col justify-center items-center bg-gray-100 w-36 h-32' onClick={() => navigate('/dice')} >
+            <img src="https://fruitfruit.s3.ap-northeast-2.amazonaws.com/icon/icon-dice.png" className='w-11 h-11 mb-3' />
+            <p>주사위 게임</p>
+            <p>기다리는 중</p>
+          </div>
+          <div className='border border-gray-200 rounded-2xl flex flex-col justify-center items-center bg-gray-100 w-36 h-32' onClick={() => navigate('/dice')} >
+            <img src="https://fruitfruit.s3.ap-northeast-2.amazonaws.com/icon/icon-mbti.png" className='w-11 h-11 mb-3' />
+            <p>친구 성향 분석</p>
+            <p>완료</p>
+          </div>
+        </div>
       </div>
     </div>
   );
