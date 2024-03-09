00401d6dc0d43495a3c7225310f4af1cf64877e9
tori <sangeun99@khu.ac.kr>
2024-03-09 11:30:50 +0900

    feat: 경험치 페이지 디자인

--- src/components/IconMenu/FriendMenu.jsx
+++ src/components/IconMenu/FriendMenu.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-function FriendMenu({ icons, onIconClick }) {
+function FriendMenu({ icons }) {
   return (
     <div className="flex flex-wrap gap-4 mt-3 justify-center">
       {icons.map((icon) => (
         <div key={icon.name} className="flex flex-col items-center">
-          <div
-            onClick={() => onIconClick(icon.name)}
-            className="rounded-full bg-white border border-gray-200">
+          <div className="rounded-full bg-white border border-gray-200">
             <img src={icon.src} alt={icon.label} className="w-16 h-16" />
           </div>
         </div>
--- src/pages/FriendGame/GetExp.jsx
+++ src/pages/FriendGame/GetExp.jsx
@@ -2,37 +2,27 @@
  * @author 엄상은
  * @email sangeun.e.9@gmail.com
  * @create date 2024-03-03 14:37:25
- * @modify date 2024-03-03 14:37:25
+ * @modify date 2024-03-09 10:46:19
  * @desc 경험치 얻는 페이지
  */
 
-import React, { useEffect, useState } from 'react';
-import startHeendy from 'assets/images/startHeendy.png';
-import SpeechBubble from 'components/SpeechBubble/SpeechBubble';
+import React from 'react';
+import ProgressBar from 'components/ModalBubbleContent/ProgressBar';
 
 function GetExp({ type, start, end }) {
   const imageUrl = `https://fruitfruit.s3.ap-northeast-2.amazonaws.com/dice/exp-${type}.png`;
 
-  const [width, setWidth] = useState(`${start}%`);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setWidth(`${end}%`);
-    }, 500);
-
-    return () => clearTimeout(timer);
-  }, [end]);
-
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
-      <SpeechBubble addStyle={"flex flex-col justify-center items-center h-2/6"} boldText={"친구와 게임하고 경험치를 얻었어"}>
-        <img src={imageUrl} alt="경험치" className="h-44" />
-        <div className='relative w-32 h-2.5 bg-black'>
-          <div className='absolute left-0 top-0 h-full bg-white transition-all duration-1000 ease-in-out' style={{ width }}></div>
+    <div className="h-full">
+      <div className="bg-[url('assets/images/heendy-background.png')] h-full">
+        <div className="w-56 mx-auto pt-56">
+          <img src={imageUrl} className='mb-8'/>
+          <ProgressBar fromValue={start} toValue={end} />
+          <div className="grid grid-rows-1 grid-flow-col grid-cols-2 mb-6 items-end text-gray-300 font-bold">
+            <span className='text-left'>Level 1</span>
+            <span className='text-right'>Level 2</span>
+          </div>
         </div>
-      </SpeechBubble>
-      <div className="flex justify-center items-center h-56">
-          <img className="h-full" src={startHeendy} />
       </div>
     </div>
   );
