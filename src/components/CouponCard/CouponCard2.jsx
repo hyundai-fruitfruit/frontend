ec010edfa003b346a5adfc8c57dd871478009d4c
tori <sangeun99@khu.ac.kr>
2024-03-14 15:07:36 +0900

    fix: mbti 워드 브레이크 수정

--- src/components/CouponCard/CouponCard2.jsx
+++ src/components/CouponCard/CouponCard2.jsx
@@ -1,33 +1,35 @@
-/**
- * @author 최성혁
- * @email [cinemay33@gmail.com]
- * @create date 2024-03-02 15:27:22
- * @modify date 2024-03-02 15:27:22
- * @desc Mbti 카드 컴포넌트
- */
 import React, { useState } from 'react';
 
 function CouponCard2({ imgSrc, detail, addStyle, onClick }) {
   const [isClicked, setIsClicked] = useState(false);
 
-  
   const handleClick = () => {
     setIsClicked(true); 
-    // 페이지 전환 지연
     setTimeout(() => {
       onClick(); 
       setIsClicked(false); 
     }, 200); 
   };
 
-
   const clickedStyle = isClicked ? 'scale-95' : ''; 
+
+  // detail 문자열 내의 \n을 기반으로 줄바꿈 처리
+  const renderDetail = () => {
+    return detail.split('\n').map((line, index) => (
+      // React에서 key는 각 요소를 식별하기 위해 필요합니다.
+      // 여기서는 간단히 index를 key로 사용합니다.
+      <React.Fragment key={index}>
+        {line}
+        {index < detail.split('\n').length - 1 && <br />}
+      </React.Fragment>
+    ));
+  };
 
   return (
     <div className={`flex flex-row items-center border rounded-2xl my-4 ${addStyle} ${clickedStyle}`} onClick={handleClick}>
       <img src={imgSrc} className="h-[4vh] mx-2 ml-6" /> 
-      <div className="flex flex-col justify-center mx-6 my-10 ml-2"> 
-        <p className="font-bold text-center">{detail}</p>
+      <div className="flex flex-col justify-center mx-6 my-10 ml-2 grow"> 
+        <p className="font-bold text-center">{renderDetail()}</p>
       </div>
     </div>
   );
