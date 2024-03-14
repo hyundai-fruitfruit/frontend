eb174a682596da5f083fab381f295368c4998b80
osy <osy9757@gmail.com>
2024-03-15 08:36:13 +0900

    feat : add icon

--- src/pages/MyRoom/OptionIcons.js
+++ src/pages/MyRoom/OptionIcons.js
@@ -2,19 +2,24 @@
  * @author 황수영
  * @email sooyoung.h8@gmail.com
  * @create date 2024-02-28 04:35:36
- * @modify date 2024-02-28 04:35:36
+ * @modify date 2024-03-15 08:35:58
  * @desc 나의 방탭의 옵션 컴포넌트
  */
 import React from 'react';
 
 const OptionIcons = ({ options, setSelectedOption }) => {
   return (
-    <div className='icon-container'>
-      {options.map(option => (
+    <div className="icon-container">
+      {options.map((option) => (
         <div
-          className='icon bg-gray-300 m-2 mb-4 w-15vw h-15vw rounded-lg'
+          className="icon bg-gray-300 m-2 mb-4 w-15vw h-15vw rounded-lg"
           key={option.name}
-          style={{ backgroundImage: `url(${option.backgroundImageUrl})` }}
+          style={{
+            backgroundImage: `url(${option.icon})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+          }}
           onClick={() => setSelectedOption(option.name)}
         >
           {/* {option.name} */}
